5dcb91ec0e48f2ae516713c4c6b452df531ef8c6
jdawsonimgix <jdawson@imgix.com>
2022-12-14 11:51:48 -0800

    Added frontend and backend. Calls on prewritten request for images tagged Cup. I need to add search functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchArray, setSearchArray] = useState();
+
+  useEffect(() => {}, [searchArray]);
+
+  const getMovies = async (sentSearchTerm) => {
+    let searchAxios = await axios
+      .get("http://localhost:5001/searching")
+      .then(function (response) {
+        return response;
+      })
+      .catch(function (error) {
+        console.log(error);
+        return error;
+      });
+
+    console.log(searchAxios.data);
+    setSearchArray(searchAxios.data);
+    // console.log(searchAxios.data.data);
+    // console.log(console.log(searchAxios.data.data.length));
+
+    // console.log(searchAxios.data.data[0].attributes.origin_path);
+    // tempArray.push(searchAxios.data.data[0].attributes.origin_path);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) {
+      getMovies(searchTerm);
+      setSearchTerm("");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const testReturnResult = () => {
+    console.log(searchArray);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <button onClick={testReturnResult}>test</button>
+        <h1>search</h1>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className='search'
+            type='search'
+            placeholder='Search movie title'
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
       </header>
     </div>
   );
--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+const port = 5001;
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/searching", async (req, res) => {
+  console.log("Checking imgix status in /testsearch");
+  let arrSearchImgix = [];
+
+  var config = {
+    method: "get",
+    url: "https://api.imgix.com/api/v1/assets/622f76522d67dbae5fb46268?filter[tags]=cup",
+    headers: {
+      Authorization: "Bearer " + process.env.IMGIX_API,
+      "Content-Type": "application/vnd.api+json",
+    },
+  };
+
+  let grabAll = await axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      for (var i = 0; i < response.data.data.length; i++) {
+        arrSearchImgix.push(response.data.data[i].attributes.origin_path);
+      }
+      return response.data;
+    })
+    .catch(function (error) {
+      console.log(error);
+      return error;
+    });
+
+  return res.status(200).send(arrSearchImgix);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+//
